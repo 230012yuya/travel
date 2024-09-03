44a5f5849e261faf1a492a56546479f92a79f4bb
230012yuya <230012nx@yse-c.net>
2024-09-03 13:07:12 +0900

    ほほいひおｈ

--- view_plans.php
+++ view_plans.php
@@ -87,18 +87,6 @@ $result = $conn->query($sql);
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="home.php">ホーム</a></li>
-                <li><a href="create_plan.php">旅行プラン作成</a></li>
-                <li><a href="view_plans.php">旅行プラン表示</a></li>
-                <li><a href="profile.php">プロフィール</a></li>
-                <li><a href="past_plans.php">過去の旅行プラン</a></li>
-                <li><a href="logout.php">ログアウト</a></li>
-            </ul>
-        </nav>
-    </header>
     <main>
         <h1>旅行プラン表示</h1>
         <ul>
